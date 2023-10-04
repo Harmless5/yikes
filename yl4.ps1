21ada257e1a5a347d11a36119d87b71c523a63a2
Harmless5 <kristjan.kruus.001@khk.ee>
2023-10-04 12:50:25 +0300

    asd

---
+++ yl4.ps1
@@ -0,0 +1,27 @@
+# Skript, mis kustutab Windowsi kasutaja nimega "eesnimi.perenimi", küsides kasutajalt kasutajanime.
+
+# Skript, mis kustutab Windowsi kasutaja nimega "eesnimi.perenimi"
+# Küsi kasutaja nime
+
+Write-Host "1 - Kustuta kasutades nime: (John Doe)"
+Write-Host "2 - Kustuta kasutades kasutajanime: (john.doe)"
+Write-Host "3 - Naita kasutajaid"
+$valik = Read-Host "Vali number"
+
+if ($valik -eq 1) {
+    $kasutaja = Read-Host "Sisesta nimi"
+    $kasutaja = $kasutaja.Split(" ")
+    $eesnimi = $kasutaja[0]
+    $perenimi = $kasutaja[1]
+    $lowerkasutaja = $eesnimi.ToLower() + "." + $perenimi.ToLower()
+    Write-Host $lowerkasutaja
+    Remove-ADUser -Name $lowerkasutaja
+    Write-Host "Kasutaja $lowerkasutaja kustutatud!"
+} elseif ($valik -eq 2) {
+    $kasutaja = Read-Host "Sisesta kasutaja nimi"
+    $lowerkasutaja = $kasutaja.ToLower()
+    Remove-ADUser -Name $lowerkasutaja
+    Write-Host "Kasutaja $lowerkasutaja kustutatud!"
+} elseif ($valik -eq 3) {
+    Get-ADUser
+}
