2e1fdcf6b83d39c2a255a2ad50d1896158e2f8a9
Harmless5 <kristjan.kruus.001@khk.ee>
2023-10-04 08:58:29 +0300

    asd

--- powershell/yl2.ps1
+++ powershell/yl2.ps1
@@ -1,12 +1,16 @@
 # Skript, mis kustutab Windowsi kasutaja nimega "eesnimi.perenimi"
 # Küsi kasutaja nime
 
+
+
+
 Write-Host "1 - Kustuta kasutades nime: (John Doe)"
 Write-Host "2 - Kustuta kasutades kasutajanime: (john.doe)"
+Write-Host "3 - Näita kasutajaid"
 $valik = Read-Host "Vali number"
 
 if ($valik -eq 1) {
-    Write-Host "Valisid 1"
+    #Write-Host "Valisid 1"
     $kasutaja = Read-Host "Sisesta nimi"
     $kasutaja = $kasutaja.Split(" ")
     $eesnimi = $kasutaja[0]
@@ -15,10 +19,12 @@ if ($valik -eq 1) {
     Write-Host $lowerkasutaja
     Remove-LocalUser -Name $lowerkasutaja
     Write-Host "Kasutaja $lowerkasutaja kustutatud!"
-} else {
-    Write-Host "Valisid 2"
+} elseif ($valik -eq 2) {
+    #Write-Host "Valisid 2"
     $kasutaja = Read-Host "Sisesta kasutaja nimi"
     $lowerkasutaja = $kasutaja.ToLower()
     Remove-LocalUser -Name $lowerkasutaja
     Write-Host "Kasutaja $lowerkasutaja kustutatud!"
+} elseif ($valik -eq 3) {
+    Get-LocalUser
 }
