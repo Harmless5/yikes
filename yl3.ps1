44859b3085ae4be62057826bf9864689979602f1
Harmless5 <kristjan.kruus.001@khk.ee>
2023-10-04 12:39:55 +0300

    asd

--- yl3.ps1
+++ yl3.ps1
@@ -1,12 +1,19 @@
 # Skript, mis võtab nimekirjast olemas olevad nimed ja teeb need AD kasutajaks, kontrollides kas kasutaja juba on olemas
 
 # Nimekiri inimestest
-$inimesed = @("Kristjan Kirsipuu","Kalle Kukk","Malle Mand","Katlin Kask")
+$inimesed = @("Kristjan Tüür","Kalle Kökk","Malle Mänd","Kätlin Kask")
 
 # Loome AD kasutaja
 foreach ($inimene in $inimesed) {
+    # Täpitähtede asendamine
+    $inimene = $inimene.Replace("ä","a")
+    $inimene = $inimene.Replace("ö","o")
+    $inimene = $inimene.Replace("ü","u")
+    $inimene = $inimene.Replace("õ","o")
+    # ees- ja perenime eraldamine
     $eesnimi = $inimene.Split(" ")[0]
     $perenimi = $inimene.Split(" ")[1]
+    # kasutajanime loomine
     $kasutaja = $eesnimi.ToLower() + "." + $perenimi.ToLower()
     if (Get-ADUser -Filter {SamAccountName -eq $kasutaja}) {
         Write-Host "Kasutaja $kasutaja on juba olemas!" -ForegroundColor red
